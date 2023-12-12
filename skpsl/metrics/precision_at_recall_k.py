e987ea8f3fb752f98b042109c8352100daa52877
Stefan <stefan.heid@lmu.de>
2023-12-12 15:08:23 +0100

    updated precision at recall to return decision threshold

--- skpsl/metrics/precision_at_recall_k.py
+++ skpsl/metrics/precision_at_recall_k.py
@@ -1,14 +1,44 @@
+from operator import itemgetter
+
 from sklearn.metrics import precision_recall_curve
 
 
-def precision_at_recall_k_score(y_true, y_prob, *, recall_level=0.9):
-    prec = max(
-        p
-        for p, r, _ in zip(*precision_recall_curve(y_true, y_prob))
-        if r >= recall_level
+def precision_at_recall_k_score(
+    y_true, y_prob, *, recall_level=0.9, return_threshold=False
+):
+    # maximum precision for a given recall level
+    prec, threshold = max(
+        (
+            (p, t)
+            for p, r, t in zip(*precision_recall_curve(y_true, y_prob))
+            if r >= recall_level
+        ),
+        key=itemgetter(0),
     )
-    return prec
+    if return_threshold:
+        return prec, threshold
+    else:
+        return prec
 
 
 if __name__ == "__main__":
-    print(precision_at_recall_k_score([1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 1]))
+    import numpy as np
+    from pytest import approx
+    from sklearn.metrics import precision_score
+
+    p, t = precision_at_recall_k_score(
+        [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+        np.linspace(0, 1, 16),
+        recall_level=0.5,
+        return_threshold=True,
+    )
+    assert p == approx(0.833, 0.01)
+    p = precision_score(
+        [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1], np.linspace(0, 1, 16) >= t
+    )
+    assert p == approx(0.833, 0.01)
+    p = precision_at_recall_k_score(
+        [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+        np.linspace(0, 1, 16),
+    )
+    assert p == approx(0.615, 0.01)
